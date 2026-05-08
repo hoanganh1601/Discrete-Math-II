d19593d250fd25d75b01c593c36ed881272efd41
HoangAnh <hoanganh1612006@gmail.com>
2026-05-08 10:22:47 +0700

    Graph-13

---
+++ TRR1013.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+using ll = long long;
+
+int main() {
+
+    ios_base :: sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    // freopen("DT.INP", "r", stdin);
+    // freopen("DT.OUT", "w", stdout);
+
+    int t; cin >> t;
+    int vertex; cin >> vertex;
+    int mtx[vertex + 1][vertex + 1];
+
+    for (int i = 1; i <= vertex; ++i) {
+        for (int j = 1; j <= vertex; ++j) cin >> mtx[i][j];
+    }
+
+    if(t == 1) {
+        vector<int> degIn(vertex + 1, 0), degOut(vertex + 1, 0);
+
+        for (int i = 1; i <= vertex; ++i) {
+            for (int j = 1; j <= vertex; ++j) {
+                degIn[j] += mtx[i][j];
+                degOut[i] += mtx[i][j];
+            }
+        }
+
+        for (int i = 1; i <= vertex; ++i) {
+            cout << degIn[i] << " " << degOut[i] << endl;
+        }
+    }
+
+    else {
+        vector<int> adj[vertex + 1];
+        for (int i = 1; i <= vertex; ++i) {
+            for (int j = 1; j <= vertex; ++j) {
+                if(mtx[i][j]) adj[i].push_back(j);
+            }
+        }
+
+        cout << vertex << endl;
+        for (int i = 1; i <= vertex; ++i) {
+            cout << adj[i].size() << " ";
+            for (int x : adj[i]) cout << x << " ";
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
