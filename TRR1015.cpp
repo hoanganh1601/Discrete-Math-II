918d293de3938f54230a230ea2eede119e7f8e6d
HoangAnh <hoanganh1612006@gmail.com>
2026-05-08 10:45:18 +0700

    Graph-15

---
+++ TRR1015.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+using ll = long long;
+
+int main() {
+
+    ios_base :: sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    // freopen("DT.INP", "r", stdin);
+    // freopen("DT.OUT", "w", stdout);
+
+    int t; cin >> t;
+    int vertex, edge; cin >> vertex >> edge;
+    vector<vector<int>> mtx(vertex + 1, vector<int>(vertex + 1));
+    vector<int> degIn(vertex + 1, 0), degOut(vertex + 1, 0);
+
+    for (int i = 1; i <= edge; ++i) {
+        int u, v; cin >> u >> v;
+        mtx[u][v] = 1;
+        ++degIn[v];
+        ++degOut[u];
+    }
+
+    if(t == 1) {
+        for (int i = 1; i <= vertex; ++i) {
+            cout << degIn[i] << " " << degOut[i] << endl;    
+        }
+    }
+
+    else {
+        cout << vertex << endl;
+        for (int i = 1; i <= vertex; ++i) {
+            for (int j = 1; j <= vertex; ++j) {
+                cout << mtx[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
