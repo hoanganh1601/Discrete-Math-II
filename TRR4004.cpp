0d1ca20e9ce5dfe49fe3ce5d956941008c569622
HoangAnh <hoanganh1612006@gmail.com>
2026-06-13 23:59:49 +0700

    Prim (update)

--- TRR4004.cpp
+++ TRR4004.cpp
@@ -4,47 +4,60 @@
 using namespace std;
 using ll = long long;
 
-int vertex, s;
-int arr[101][101];
-bool taken[101];
-vector<pair<int, int>> adj[101];
-vector<pair<int, int>> res;
+struct Edge {
+    int u, v, w;
+    
+    bool operator > (const Edge& other) const {
+        if(w != other.w) return w > other.w;
+        if(u != other.u) return u > other.u;
+        return v > other.v;
+    }
+};
+
+int vertex, start;
+int mtx[105][105];
+vector<Edge> adj[105];
+vector<Edge> res;
+bool taken[105];
+
 int ans = 0;
 
-void Prim(int s) {
-    priority_queue<pair<int, pair<int, int>>, 
-                    vector<pair<int, pair<int, int>>>, 
-                    greater<pair<int, pair<int, int>>>> q;
-    taken[s] = true;
+void Prim(int start) {
+    priority_queue<Edge, vector<Edge>, greater<Edge>> q;
+    taken[start] = true; // Đỉnh xuất phát
 
-    for (pair<int, int> tmp : adj[s]) {
-        if(!taken[tmp.second]) {
-            q.push({tmp.first, {s, tmp.second}});
+    // Đẩy tất cả các đỉnh có thể 
+    for (Edge tmp : adj[start]) {
+        if(!taken[tmp.v]) {
+            q.push(tmp);
         }
     }
 
-    // duyet
     while(!q.empty()) {
-        pair<int, pair<int, int>> tmp = q.top(); q.pop();
-        int u = tmp.second.first, v = tmp.second.second, weight = tmp.first;
+        Edge cur = q.top(); q.pop();
 
+        int u = cur.u, v = cur.v, w = cur.w;
         if(!taken[v]) {
-
             taken[v] = true;
-            ans += weight;
-            res.push_back({min(u, v), max(u, v)});
+            ans += w;
 
-            for (auto pa : adj[v]) {
-                if(!taken[pa.second]) {
-                    q.push({pa.first, {v, pa.second}});
+            res.push_back({min(u, v), max(u, v), w});
+            for (Edge next : adj[v]) {
+                if(!taken[next.v]) {
+                    q.push(next);
                 }
             }
-        }  
+        }
+    }
+
+    if(res.size() < vertex - 1) {
+        cout << 0;
+        return;
     }
 
     cout << ans << endl;
-    for (auto x : res) {
-        cout << x.first << " " << x.second << " " << arr[x.first][x.second] << endl;
+    for (Edge x : res) {
+        cout << x.u << " " << x.v << " " << x.w << endl;
     }
 }
 
@@ -56,18 +69,20 @@ int main() {
     // freopen("CK.INP", "r", stdin);
     // freopen("CK.OUT", "w", stdout);
 
-    cin >> vertex >> s;
+    cin >> vertex >> start;
+    res.reserve(10005);
     for (int i = 1; i <= vertex; ++i) {
         for (int j = 1; j <= vertex; ++j) {
-            cin >> arr[i][j];
-            if(arr[i][j] > 0 && arr[i][j] <= 50) {
-                adj[i].push_back({arr[i][j], j});
+            cin >> mtx[i][j];
+            if(mtx[i][j] > 0 && mtx[i][j] <= 50) {
+                adj[i].push_back({i, j, mtx[i][j]});
             }
         }
     }
+
     memset(taken, false, sizeof(taken));
 
-    Prim(s);
+    Prim(start);
 
     return 0;
 }
